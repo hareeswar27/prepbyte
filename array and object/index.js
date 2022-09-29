217092566853827fab90d5d49a230e43b9b2c7c2
hareeswar27 <109612921+hareeswar27@users.noreply.github.com>
2022-09-29 17:40:17 +0530

    xxw

---
+++ array and object/index.js
@@ -0,0 +1,7 @@
+let studentRecords = [{name:"jon",class:'6',id:123},
+{name:"jon",class:'7',id:1231},
+{name:"hari",class:'8',id:1234},
+{name:"naz",class:'9',id:12345},
+{name:"lal",class:'10',id:123231},
+{name:"ram",class:'lkg',id:123122}]
+console.log(studentRecords);
